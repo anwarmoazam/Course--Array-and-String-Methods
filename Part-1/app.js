484dcd62dd1aa38147b5ea99ec9b6e414b0bbad6
anwarmoazam <anwar.palimarwar@gmail.com>
2023-06-02 21:25:15 +0530

    array methods

--- Part-1/app.js
+++ Part-1/app.js
@@ -60,6 +60,11 @@ const products = [
         name: 'phone',
         price: 500,
         count: 10
+    },
+    {
+        name: 'calculator',
+        price: 100,
+        count: 500
     }
 ]
 
@@ -175,20 +180,23 @@ function storeTotal(acc, curr) {
 console.log(totalAmount);
 
 // slice Array Method - Original array not change
+// Syntax = arr.slice(start index, end index) -- (end index not included and it is optional)
 
 const numbersSliced = numbers.slice(1,4);
 
 console.log('numbers : ',numbers);
 console.log('numberSliced : ',numbersSliced);
 
-const numbersSliced_1 = numbers.slice();
+const numbersSliced_1 = numbers.slice(); //returns array shallow copy from index to index
+
+console.log('numberSliced_1 : ',numbersSliced_1);
 
 numbersSliced_1[1] = 10;
 
 console.log('numbers : ',numbers);
 console.log('numberSliced_1 : ',numbersSliced_1);
 
-const numbersSliced_2 = numbers.slice(-1);
+const numbersSliced_2 = numbers.slice(-2); //it will start from the end and copy last two element in new array
 console.log('numbers : ',numbers);
 console.log(numbersSliced_2);
 
@@ -198,12 +206,90 @@ const winners = participants.slice(0,2);
 
 console.log('winners : ',winners);
 
-// splice Array Method - to remove or insert element in original array
+// splice Array Method - to remove or insert element in original array (It Modifies original array)
+// Syntax = arr.splice(<start index>,<how many items you want to remove>,<add new items (Optional)>);
+
+console.log('numbers : ',numbers);
+
 let deleted = numbers.splice(2,1);
 
-console.log('numbers : ',numbers);
+console.log('numbers array after splice : ',numbers);
 console.log('deleted : ',deleted);
 
-deleted = numbers.splice(2,0,10,20);
-console.log('numbers : ',numbers);
-console.log('deleted : ',deleted);
+deleted = numbers.splice(2,0,10,20); //deleted array will be empty because we pass 0 as delete elements
+
+console.log('new elements added array : ',numbers);
+console.log('deleted array : ',deleted);
+
+// sort Array method (sorts elements of an original array) 
+// -- the array sort method sorts the elements of an array by manipulating the array.
+// -- the default sort order is ascending
+// -- sort method converts elements in string first
+
+const names = ['Florin','Liam','Jai','Ivan'];
+
+console.log('names array before sort : ',names);
+
+names.sort();
+
+console.log('names array after sort : ',names);
+
+const numbersArray = [74,18,10,5,84,24,105];
+
+console.log('numbersArray before sort : ',numbersArray);
+
+// numbersArray.sort();
+
+
+function compareFunction(a,b){
+    // 1. <0 ... a comes first
+    // 2. 0 ... nothing will be changed
+    // 3. >0 ... b comes first
+    console.log(`a=${a}, b=${b} and ${a}-${b} = ${a - b}`);
+    return a-b;
+}
+
+numbersArray.sort(compareFunction);
+
+console.log('numbersArray after sort : ',numbersArray);
+
+console.log('Before sort products array : ');
+for(let product of products){
+    console.log(product)
+}
+
+products.sort((a,b) => a.price - b.price);
+
+console.log('After sort by price products array : ');
+for(let product of products){
+    console.log(product)
+}
+
+products.sort((a,b) => a.count - b.count);
+
+console.log('After sort by count products array : ');
+for(let product of products){
+    console.log(product)
+}
+
+// doubt in sort by name
+products.sort((a,b) => a.name - b.name);
+
+console.log('After sort by name products array : ');
+for(let product of products){
+    console.log(product)
+}
+
+// concate Array method -- returns new Array, original array not change
+// Syntax -- arr.concat(<value> or <array>,,,,,)
+
+const a = [1,2,3];
+const b = [4,5,6];
+
+const c = a.concat(b);
+
+console.log(a);
+console.log(b);
+console.log(c);
+
+// fill Array method
