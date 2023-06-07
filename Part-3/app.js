db555d4cb58211ba9d4570fc29ec5c1941e3041c
anwarmoazam <anwar.palimarwar@gmail.com>
2023-06-08 01:32:24 +0530

    String methods

---
+++ Part-3/app.js
@@ -0,0 +1,12 @@
+// String.charAt(index) method
+
+const string = 'Code Explained';
+
+const charAt5 = string.charAt(5);
+
+console.log(charAt5);
+
+// String.indexOf(searchString,position) and String.lastIndexOf(searchString,position) method
+
+const s = 'The code undefined code code !';
+
