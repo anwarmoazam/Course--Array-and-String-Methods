e1e229a98f8167b043f4e7bca9c1e2669a444875
anwarmoazam <anwar.palimarwar@gmail.com>
2023-06-08 18:24:00 +0530

    String methods

--- Part-3/app.js
+++ Part-3/app.js
@@ -109,3 +109,99 @@ console.log(splitResult4);
 let splitResult5 = str1.split("o",2) //set limit 1 with seperator o
 console.log(splitResult5);
 
+// includes method - this method is used to string found in original string
+// Syntax : string.includes(searchString, Position)
+
+const sentance = 'The quick brown fox jumps over the lazy dog.';
+
+let resultIncludes = sentance.includes('fox');
+
+console.log(resultIncludes);
+
+resultIncludes = sentance.includes('Fox');
+
+console.log(resultIncludes);
+
+resultIncludes = sentance.includes('fox',17);
+
+console.log(resultIncludes);
+
+// startsWith - this method is used whenever you want to check if a string start with another string or not
+// startsWith and endsWith method - this method is used whenever you want to check if a string ends with another string or not
+
+// Syntax : string.startsWith(searchString, Position);
+
+const str2 = 'Cats are the best!';
+
+let resultStartsWith = str2.startsWith('Cats'); //case sensative
+
+console.log(resultStartsWith);
+
+resultStartsWith = str2.startsWith('are',5); 
+
+console.log(resultStartsWith);
+
+// Syntax : string.endsWith(searchString, length);
+
+let resultEndsWith = str2.endsWith('best!'); //case sensative
+
+console.log(resultEndsWith);
+
+resultEndsWith = str2.endsWith('best',17); 
+
+console.log(resultEndsWith);
+
+// concat method - concate is used to concatenate a string (doesn't modified any of the string)
+// Syntax : string.concat(string2, string3, .... stringN);
+
+const str3 = 'Hello', str4 = 'World';
+
+let concateResult = str3.concat(' ',str4,' ',str2);
+
+console.log(concateResult);
+
+// repeat method - this method is used to whenever you want to more copies a string
+// Syntax : string.repeat(count) -- count must be greater than or equal to zero
+
+const str5 = 'abc';
+
+let repeatResult = str5.repeat(0);
+console.log(repeatResult);
+
+repeatResult = str5.repeat(1);
+console.log(repeatResult);
+
+repeatResult = str5.repeat(2);
+console.log(repeatResult);
+
+repeatResult = str5.repeat(3.5);
+console.log(repeatResult);
+
+// repeatResult = str5.repeat(-1);
+console.log(repeatResult);
+
+// trim, trimStart and trimEnd methods - these methods here are used whenever you want to remove whitespace characters and not changes the original string
+
+// Syntax : string.trim() - removes whitespace from start and end
+
+const str6 = '       Hello world              ';
+
+let trimResult = str6.trim();
+
+console.log(trimResult);
+
+// Syntax : string.trimStart() - removes whitespace from start
+
+const str7 = '       Hello world              ';
+
+let trimStartResult = str7.trimStart();
+
+console.log(trimStartResult);
+
+// Syntax : string.trimEnd() - removes whitespace from end
+
+const str8 = '       Hello world              ';
+
+let trimEndResult = str8.trimEnd();
+
+console.log(trimEndResult);
