e589be11fa0142df29c1ed9f90d62871c18e0db4
anwarmoazam <anwar.palimarwar@gmail.com>
2023-06-09 00:57:26 +0530

    string methods

--- Part-3/app.js
+++ Part-3/app.js
@@ -13,32 +13,32 @@ const s = 'The code undefined code code !';
 let resultIndexOf = s.indexOf('Code');
 let resultLastIndexOf = s.lastIndexOf('Code');
 
-console.log({resultIndexOf,resultLastIndexOf});
+console.log({ resultIndexOf, resultLastIndexOf });
 
 resultIndexOf = s.indexOf('code');
 resultLastIndexOf = s.lastIndexOf('code');
 
-console.log({resultIndexOf,resultLastIndexOf});
+console.log({ resultIndexOf, resultLastIndexOf });
 
-resultIndexOf = s.indexOf('code',5);
-resultLastIndexOf = s.lastIndexOf('code',23);
+resultIndexOf = s.indexOf('code', 5);
+resultLastIndexOf = s.lastIndexOf('code', 23);
 
-console.log({resultIndexOf,resultLastIndexOf});
+console.log({ resultIndexOf, resultLastIndexOf });
 
-resultIndexOf = s.indexOf('code',-5);
-resultLastIndexOf = s.lastIndexOf('code',-5);
+resultIndexOf = s.indexOf('code', -5);
+resultLastIndexOf = s.lastIndexOf('code', -5);
 
-console.log({resultIndexOf,resultLastIndexOf});
+console.log({ resultIndexOf, resultLastIndexOf });
 
 resultIndexOf = s.indexOf('');
 resultLastIndexOf = s.lastIndexOf('');
 
-console.log({resultIndexOf,resultLastIndexOf});
+console.log({ resultIndexOf, resultLastIndexOf });
 
 resultIndexOf = s.indexOf(); //because our string contains word undefined it returns undefined word index otherwise it returns -1
 resultLastIndexOf = s.lastIndexOf(); //because our string contains word undefined it returns undefined word index otherwise it returns -1
 
-console.log({resultIndexOf,resultLastIndexOf});
+console.log({ resultIndexOf, resultLastIndexOf });
 
 // String.slice() and String.substring method
 
@@ -50,42 +50,42 @@ const str1 = 'The morning is upon us.'
 let resultSlice = str1.slice(12);
 let resultSubstring = str1.substring(12);
 
-console.log({resultSlice,resultSubstring});
+console.log({ resultSlice, resultSubstring });
 
 resultSlice = str1.slice(-13); //if negative number passed then count from last
 resultSubstring = str1.substring(-12); //if negative number passed then it will be convert 0 any negative number
 
-console.log({resultSlice,resultSubstring});
+console.log({ resultSlice, resultSubstring });
 
 resultSlice = str1.slice(str1.length); //if negative number passed then count from last
 resultSubstring = str1.substring(str1.length); //if negative number passed then it will be convert 0 any negative number
 
-console.log({resultSlice,resultSubstring});
+console.log({ resultSlice, resultSubstring });
 
-resultSlice = str1.slice(13,16); 
-resultSubstring = str1.substring(13,16);
+resultSlice = str1.slice(13, 16);
+resultSubstring = str1.substring(13, 16);
 
-console.log({resultSlice,resultSubstring});
+console.log({ resultSlice, resultSubstring });
 
-resultSlice = str1.slice(16,13); 
-resultSubstring = str1.substring(16,13); //if indexEnd small than indexStart then substring method will swap them
+resultSlice = str1.slice(16, 13);
+resultSubstring = str1.substring(16, 13); //if indexEnd small than indexStart then substring method will swap them
 
-console.log({resultSlice,resultSubstring});
+console.log({ resultSlice, resultSubstring });
 
-resultSlice = str1.slice(-8,-4); // count from last of string
-resultSubstring = str1.substring(-8,-4); //if negative number then substring method will converts them into 0 so indexStart = 0 and indexEnd = 0
+resultSlice = str1.slice(-8, -4); // count from last of string
+resultSubstring = str1.substring(-8, -4); //if negative number then substring method will converts them into 0 so indexStart = 0 and indexEnd = 0
 
-console.log({resultSlice,resultSubstring});
+console.log({ resultSlice, resultSubstring });
 
-resultSlice = str1.slice(-8,4); 
-resultSubstring = str1.substring(-8,4); 
+resultSlice = str1.slice(-8, 4);
+resultSubstring = str1.substring(-8, 4);
 
-console.log({resultSlice,resultSubstring});
+console.log({ resultSlice, resultSubstring });
 
-resultSlice = str1.slice(8,-4); 
-resultSubstring = str1.substring(8,-4); 
+resultSlice = str1.slice(8, -4);
+resultSubstring = str1.substring(8, -4);
 
-console.log({resultSlice,resultSubstring});
+console.log({ resultSlice, resultSubstring });
 
 // split method - return substring using seperator in an array
 // Syntax : string.split(seperator, limit)
@@ -94,7 +94,7 @@ let splitResult1 = str1.split() //when no seperator is used the entire string re
 
 console.log(splitResult1);
 
-let splitResult2 = str1.split(" ") 
+let splitResult2 = str1.split(" ")
 
 console.log(splitResult2);
 
@@ -102,11 +102,11 @@ let splitResult3 = str1.split("") //empty string is everywhere
 
 console.log(splitResult3);
 
-let splitResult4 = str1.split("o") 
+let splitResult4 = str1.split("o")
 
 console.log(splitResult4);
 
-let splitResult5 = str1.split("o",2) //set limit 1 with seperator o
+let splitResult5 = str1.split("o", 2) //set limit 1 with seperator o
 console.log(splitResult5);
 
 // includes method - this method is used to string found in original string
@@ -122,7 +122,7 @@ resultIncludes = sentance.includes('Fox');
 
 console.log(resultIncludes);
 
-resultIncludes = sentance.includes('fox',17);
+resultIncludes = sentance.includes('fox', 17);
 
 console.log(resultIncludes);
 
@@ -137,7 +137,7 @@ let resultStartsWith = str2.startsWith('Cats'); //case sensative
 
 console.log(resultStartsWith);
 
-resultStartsWith = str2.startsWith('are',5); 
+resultStartsWith = str2.startsWith('are', 5);
 
 console.log(resultStartsWith);
 
@@ -147,7 +147,7 @@ let resultEndsWith = str2.endsWith('best!'); //case sensative
 
 console.log(resultEndsWith);
 
-resultEndsWith = str2.endsWith('best',17); 
+resultEndsWith = str2.endsWith('best', 17);
 
 console.log(resultEndsWith);
 
@@ -156,7 +156,7 @@ console.log(resultEndsWith);
 
 const str3 = 'Hello', str4 = 'World';
 
-let concateResult = str3.concat(' ',str4,' ',str2);
+let concateResult = str3.concat(' ', str4, ' ', str2);
 
 console.log(concateResult);
 
@@ -205,3 +205,138 @@ const str8 = '       Hello world              ';
 let trimEndResult = str8.trimEnd();
 
 console.log(trimEndResult);
+
+// padStart and padEnd methods
+
+// Syntax : padStart(targetLength, padString)
+
+const str9 = 'abc';
+
+let padStartResult = str9.padStart(10, '*');
+
+console.log(padStartResult);
+
+padStartResult = str9.padStart(2);
+
+console.log(padStartResult);
+
+// Syntax : padEnd(targetLength, padString)
+
+const str10 = 'abc';
+
+let padEndResult = str10.padEnd(10, '*');
+
+console.log(padEndResult);
+
+const str11 = 'abc';
+
+padEndResult = str11.padEnd(2); //if targetLength is less than string length then original string will be returned
+
+console.log(padEndResult);
+
+// localeCompare method - this method compare two strings and returs numeric value
+
+const str12 = 'Hello world';
+const str13 = 'Anwar Moazam';
+
+let localeCompareResult = str12.localeCompare(str13);
+
+console.log(localeCompareResult);
+
+console.log('b'.localeCompare('b'));
+
+// search method - search is used to search for a match between a regular expression and the string object and it returns index.
+// Syntax : string.search(regexp)
+
+let text = 'The rain in SPAIN stays mainly in the plain';
+
+let position = text.search('ain');
+console.log(position);
+
+position = text.search(/rain/);
+console.log(position);
+
+position = text.search(/AIN/);
+console.log(position);
+
+position = text.search(/AIN/i);
+console.log(position);
+
+position = text.search(/w/);
+console.log(position);
+
+// match method - this method same as search method and must use with global regexp. it also takes regular expression and it return an array with some information 
+// Syntax : string.match(regexp)
+
+let matchResult = text.match('ain');
+console.log(matchResult);
+
+matchResult = text.match(/ain/g);
+console.log(matchResult);
+
+matchResult = text.match(/ain/gi);
+console.log(matchResult);
+
+matchResult = text.match(/w/);
+console.log(matchResult);
+
+// Syntax : string.matchAll(regexp)
+
+let matchAllResult = text.matchAll(/ain/g);
+console.log([...matchAllResult]);
+
+matchAllResult = text.matchAll(/ain/gi);
+console.log([...matchAllResult]);
+
+matchAllResult = text.matchAll(/w/g);
+console.log([...matchAllResult]);
+
+// replace and replaceAll method - used for replace string
+
+/* Syntax replace (only replace first occurance): 
+            string.replace(searchString, replacerString);
+            string.replace(searchString, replacerFunction);
+            string.replace(regexp, replacerString);
+            string.replace(regexp, replacerFunction);
+*/
+
+text = 'Mr Blue has a blue house and a blue car';
+console.log(text);
+
+let replaceResult = text.replace('blue', 'red');
+console.log(replaceResult);
+
+replaceResult = text.replace('blue', (text) => text.toUpperCase());
+console.log(replaceResult);
+
+replaceResult = text.replace(/blue/g, 'green');
+console.log(replaceResult);
+
+replaceResult = text.replace(/blue/gi, (text) => text.toUpperCase());
+console.log(replaceResult);
+
+/* Syntax replaceAll (replace all occurance in string): 
+            string.replaceAll(searchString, replacerString);
+            string.replaceAll(searchString, replacerFunction);
+            string.replaceAll(regexp, replacerString);
+            string.replaceAll(regexp, replacerFunction);
+*/
+
+text = 'Mr Blue has a blue house and a blue car';
+console.log(text);
+
+let replaceAllResult = text.replaceAll('blue', 'red');
+console.log(replaceAllResult);
+
+replaceAllResult = text.replaceAll('blue', (text) => text.toUpperCase());
+console.log(replaceAllResult);
+
+replaceAllResult = text.replaceAll(/blue/g, 'pink');
+console.log(replaceAllResult);
+
+replaceAllResult = text.replaceAll(/blue/gi, (text) => text.toUpperCase());
+console.log(replaceAllResult);
+
+replaceAllResult = text.replaceAll(/blue/g, 'red'); //replaceAll must use with global regexp otherwise shows TypeError
+console.log(replaceAllResult);
+
